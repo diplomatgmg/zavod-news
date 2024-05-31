577180061278399e90d98ccb3b4fc2b5a29dea46
Кирилл Попов <diplomatgmg@gmail.com>
2024-05-31 13:04:01 +0300

    Добавлен эндпоинт новостей по тегу

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import NewsListAPIView, TagListAPIView, NewsDetailAPIView
+from .views import NewsListAPIView, TagListAPIView, NewsDetailAPIView, NewsByTagAPIView
 
 urlpatterns = [
     path("news/", NewsListAPIView.as_view(), name="news"),
     path("news/<int:pk>/", NewsDetailAPIView.as_view(), name="news_detail"),
     path("tags/", TagListAPIView.as_view(), name="tags"),
+    path("tags/<int:pk>/news/", NewsByTagAPIView.as_view(), name="news-by-tag"),
 ]
--- backend/news/views.py
+++ backend/news/views.py
@@ -18,3 +18,12 @@ class NewsListAPIView(ListAPIView):
 class NewsDetailAPIView(RetrieveAPIView):
     queryset = News.objects.all().prefetch_related("tags")
     serializer_class = NewsDetailSerializer
+
+
+class NewsByTagAPIView(ListAPIView):
+    serializer_class = NewsDetailSerializer
+    pagination_class = NewsPagination
+
+    def get_queryset(self):
+        tag = self.kwargs.get("pk")
+        return News.objects.filter(tags__id=tag).prefetch_related("tags")
