2e594b4dbe3270a866c0a7e471ae568724921bbe
Кирилл Попов <diplomatgmg@gmail.com>
2024-05-31 20:53:19 +0300

    Убран лишний фильтр

--- frontend/src/components/SearchForm.tsx
+++ frontend/src/components/SearchForm.tsx
@@ -4,18 +4,8 @@ import { Button, Form, InputGroup, Row, Col } from 'react-bootstrap'
 const SearchForm = (): ReactElement => {
   return (
     <Form>
-      <Row>
-        <Col>
-          <InputGroup className="mb-4">
-            <Form.Control
-              placeholder="Поиск по заголовку"
-            />
-            <Button variant="outline-secondary" id="button-addon2">
-              Применить
-            </Button>
-          </InputGroup>
-        </Col>
-        <Col>
+      <Row className="justify-content-center">
+        <Col xs={5}>
           <InputGroup className="mb-3">
             <Form.Control
               placeholder="Поиск по тегам"
